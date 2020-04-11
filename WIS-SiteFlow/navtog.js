dbce31d0b08f36a051a65637591b1a538aec7e49
ak2254 <ak2254@njit.edu>
2020-04-11 18:11:21 -0400

    Initial commit

---
+++ WIS-SiteFlow/navtog.js
@@ -0,0 +1,12 @@
+
+
+
+
+function toggleClass(){
+    let menu = document.querySelector(".mainMenu");
+    menu.classList.toggle("toggleCls");
+}
+
+
+let hamburger = document.querySelector(".hamIcon");
+hamburger.addEventListener("click", toggleClass);
